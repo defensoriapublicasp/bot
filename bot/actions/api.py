fb4ffdd6887979b0037cbdff425e5a3024426688
arthurTemporim <arthurrtl@gmail.com>
2019-04-30 11:25:43 -0300

    Add token environment variable
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>

--- bot/actions/api.py
+++ bot/actions/api.py
@@ -1,13 +1,11 @@
 import requests
+import os
+token = os.environ['DEFENSORIA_TOKEN']
 
 url = 'https://online.defensoria.sp.gov.br/autoatendimento/v1/estrutura'
-headers = {'x-access-token': '43db84b5147baad505df2412f6303623'}
+headers = {'x-access-token': token}
 
 def get_city_data(city='São Paulo'):
     params = (('nomeCidade', city),)
     response = requests.get(url, headers=headers, params=params)
     return response.json()
-
-#response = get_city_data('São Paulo')
-#print(response[0]['nome'])
-#print(response[0]['endereco']['logradouro'])
